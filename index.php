f1ce657cc59351f9529437d960465c93247d15f6
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-27 09:28:52 -0200

    Acrescentando NAVBAR

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
     <title>Classificados</title>
 </head>
 <body>
-    
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="./" class="navbar-brand" >Classificados</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right" >
+            <li><a href="">Cadastre-se</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </div>
+</nav>
 </body>
 </html>
